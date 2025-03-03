675ce543482447cddbd60de665c1e44beaf2cce2
Oleg Schegol <olschegolev@yahoo.com>
2025-03-03 12:48:00 +0500

    add go command

---
+++ app/Console/Commands/GoCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Category;
+use Illuminate\Console\Command;
+
+class GoCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:go-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $category = Category::find(1);
+        dd(Category::all()->except($category->id));
+    }
+}

bf803bccdb798034dfa3e8630a9b58ab7014254a
Oleg Schegol <olschegolev@yahoo.com>
2025-03-12 16:34:17 +0500

    Rendering child products

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -2,33 +2,33 @@
 import Sidebar from "@/Components/Sidebar.vue";
 
 export default {
-  name: "AdminLayout",
+    name: "AdminLayout",
     components: {Sidebar},
 
 }
 </script>
 
 <template>
-  <section class="w-full p-4 border-b border-gray-100">
-    <header>
-      Market Admin
-    </header>
-  </section>
-  <section class="flex ">
-    <aside class="w-1/8 min-h-screen">
-      <nav>
-        <Sidebar/>
-      </nav>
-    </aside>
-    <article class="w-7/8 bg-gray-50 w-full mr-12">
-      <slot/>
-    </article>
-  </section>
-  <section>
-    <footer>
+    <section class="w-full p-4 border-b border-gray-100">
+        <header>
+            Market Admin
+        </header>
+    </section>
+    <section class="flex ">
+        <aside class="w-1/8 min-h-screen">
+            <nav>
+                <Sidebar/>
+            </nav>
+        </aside>
+        <article class="w-7/8 bg-gray-50 w-full mr-12">
+            <slot/>
+        </article>
+    </section>
+    <section>
+        <footer>
 
-    </footer>
-  </section>
+        </footer>
+    </section>
 </template>
 
 <style scoped>
